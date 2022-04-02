538195222c778d323af41becb78f01bf413167db
InChh <2319397152@outlook.com>
2022-04-02 10:56:36 +0800

    创建题库管理模块

---
+++ exam-modules/exam-modules-exercise/src/main/java/top/tran4f/exam/exercise/ExamExerciseApplication.java
@@ -0,0 +1,18 @@
+package top.tran4f.exam.exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 题库管理
+ *
+ * @author inchh
+ */
+@SpringBootApplication
+public class ExamExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamExerciseApplication.class, args);
+    }
+
+}
